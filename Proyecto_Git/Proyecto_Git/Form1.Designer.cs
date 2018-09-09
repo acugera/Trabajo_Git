7ec88bc10baf040e98b358cb039fa862e77e3716
DavidGerasimchuk <acugera@gmail.com>
2018-09-09 15:34:15 -0300

    Primer subida del proyecto en C#

---
+++ Proyecto_Git/Proyecto_Git/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Proyecto_Git
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrueva = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPrueva
+            // 
+            this.lblPrueva.AutoSize = true;
+            this.lblPrueva.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrueva.Location = new System.Drawing.Point(71, 10);
+            this.lblPrueva.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblPrueva.Name = "lblPrueva";
+            this.lblPrueva.Size = new System.Drawing.Size(290, 73);
+            this.lblPrueva.TabIndex = 0;
+            this.lblPrueva.Text = "-Prueba-";
+            this.lblPrueva.Click += new System.EventHandler(this.lblPrueva_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 92);
+            this.Controls.Add(this.lblPrueva);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrueva;
+    }
+}
+
---
+++ Proyecto_Git/Proyecto_Git/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Git
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void lblPrueva_Click(object sender, EventArgs e)
+        {
+            if  (lblPrueva.Text == "--Prueba--")
+                lblPrueva.Text = "-Prueba-";
+            else
+                lblPrueva.Text = "--Prueba--";
+        }
+    }
+}
